7546a21d2986655aa6f8b63e1f82f5c575100d8c
Robi Olise <anierobi.paul@gmail.com>
2022-11-19 16:54:18 -0600

    Attribute Set-up

--- src/LifeCoach.java
+++ src/LifeCoach.java
@@ -1,3 +1,30 @@
-public class LifeCoach {
+import java.time.LocalDate;
 
+public class LifeCoach {
+    String taskName;
+    String taskDescription;
+    String taskManager;
+    LocalDate startDate;
+    LocalDate deadline;
+
+
+    public LifeCoach (String taskName, String taskDescription, String taskManager, LocalDate startDate, LocalDate deadline) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.taskManager = taskManager;
+        this.startDate = startDate;
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return "LifeCoach{" +
+                "taskName='" + taskName + '\'' +
+                ", taskDescription='" + taskDescription + '\'' +
+                ", taskManager='" + taskManager + '\'' +
+                ", startDate=" + startDate +
+                ", deadline=" + deadline +
+                '}';
+    }
 }
+
